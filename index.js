29656cc49858dc6f838d2ca5b43eb6200230f0f1
Jakaria96 <122998075+Jakaria96@users.noreply.github.com>
2025-06-30 21:07:00 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+
+
+
+   const bShirt = document.getElementById('black-shirt')
+      bShirt.addEventListener('click', function(){
+
+   let shirtTxt = document.getElementById('shirt-text')
+         shirtTxt.style.display = 'none'
+   let shirtX = document.getElementById('shirt-x')
+         shirtX.style.display = 'none'
+   let cTxt = document.getElementById('cloudy-text')
+             cTxt.style.display = 'none'
+   let cShirtX = document.getElementById('cloudy-shirt-x')
+             cShirtX.style.display = 'none'
+
+   let bTxt = document.getElementById('black-text')
+         bTxt.style.display = 'block'
+   let bShirtX = document.getElementById('black-shirt-x')
+         bShirtX.style.display = 'block'
+
+      })
+
+   const cShirt = document.getElementById('cloudy-shirt')
+         cShirt.addEventListener('click', function(){
+
+      var shirtTxt = document.getElementById('shirt-text')
+             shirtTxt.style.display = 'none' 
+      var shirtX = document.getElementById('shirt-x')
+             shirtX.style.display = 'none'
+
+      var bTxt = document.getElementById('black-text')
+             bTxt.style.display = 'none'
+      var bShirtX = document.getElementById('black-shirt-x')
+             bShirtX.style.display = 'none'
+
+
+      var cTxt = document.getElementById('cloudy-text')
+             cTxt.style.display = 'block'
+      var cShirtX = document.getElementById('cloudy-shirt-x')
+             cShirtX.style.display = 'block'
+         })
+
+
+   const shirt = document.getElementById('Shirt')
+         shirt.addEventListener('click', function(){
+
+      var shirtTxt = document.getElementById('shirt-text')
+             shirtTxt.style.display = 'block' 
+      var shirtX = document.getElementById('shirt-x')
+             shirtX.style.display = 'block'
+      
+      var bTxt = document.getElementById('black-text')
+             bTxt.style.display = 'none'
+      var bShirtX = document.getElementById('black-shirt-x')
+             bShirtX.style.display = 'none'
+
+      let cTxt = document.getElementById('cloudy-text')
+             cTxt.style.display = 'none'
+      let cShirtX = document.getElementById('cloudy-shirt-x')
+             cShirtX.style.display = 'none'
+
+
+         })
+
+
